ea8e115bb19b84c6ba5fa88e23cf0248f61ffa87
kompisahlen-cmyk <kompis.ahlen@gmail.com>
2026-02-27 23:32:08 +0100

    Fix: Add complete Android project structure for CI build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build file
 plugins {
     id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
 buildscript {
-    extra["compose_version"] = "1.6.1"
+    // NOTE: compose_version removed - using BOM (Bill of Materials) in app/build.gradle.kts instead
     extra["room_version"] = "2.5.2"
     extra["lifecycle_version"] = "2.7.0"
 }
